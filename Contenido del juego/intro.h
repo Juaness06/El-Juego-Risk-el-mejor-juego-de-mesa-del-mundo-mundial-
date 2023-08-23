c043f524056556873af6a4ba693a8d9cad7f283d
Shermann103 <100050931+Shermann103@users.noreply.github.com>
2023-08-22 23:14:05 -0500

    Add files via upload

---
+++ Contenido del juego/intro.h
@@ -0,0 +1,21 @@
+#ifndef __INTRO__H__
+#define __INTRO__H__
+class Intro
+{
+    public:
+        static void titulo();
+        static void comandos();
+};
+#endif 
+
+
+#ifndef __INICIALIZAR__H__
+#define __INICIALIZAR__H__
+#include <vector>
+#include <string>
+class Inicializar
+{
+    public:
+        static void nueva();
+};
+#endif
